e2fba0b44b2d5ba34816ed751ba47d4a1f5840f4
Samuel Hyrum <samuelhymp@gmail.com>
2023-04-14 18:33:04 -0300

    Requisito 10, def top_5_categories

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,18 @@
+from tech_news.database import search_news
+from collections import Counter
+
+
 # Requisito 10
 def top_5_categories():
     """Seu código deve vir aqui"""
+    data = search_news({})
+
+    lista_category = [news["category"] for news in data]
+
+    without_duplicates = Counter(lista_category)
+
+    ordered_words = sorted(
+        without_duplicates.keys(), key=lambda category: (
+            -without_duplicates[category], category))
+
+    return ordered_words[:5]
